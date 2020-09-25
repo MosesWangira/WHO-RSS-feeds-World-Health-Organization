5915b94de78e745bac87a9e15feb5cb6c54e262e
MosesWangira <40572173+MosesWangira@users.noreply.github.com>
2020-09-25 19:25:51 +0300

    add splash screen animation

--- app/src/main/java/com/example/diseaseoutbreaks/ui/SplashScreen.kt
+++ app/src/main/java/com/example/diseaseoutbreaks/ui/SplashScreen.kt
@@ -1,7 +1,11 @@
 package com.example.diseaseoutbreaks.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
 import com.example.diseaseoutbreaks.R
 
 class SplashScreen : AppCompatActivity() {
@@ -9,6 +13,32 @@ class SplashScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
+        val splashAnimator = AnimationUtils.loadAnimation(
+            this,
+            R.anim.splash_animation
+        )
 
+        val splashImage: ImageView = findViewById(R.id.splashImage)
+        val splashText: TextView = findViewById(R.id.splashText)
+
+        splashImage.startAnimation(splashAnimator)
+        splashText.startAnimation(splashAnimator)
+
+
+        val toLogin = Intent(this, MainActivity::class.java)
+        //running on thread
+        val timer = object : Thread() {
+            override fun run() {
+                try {
+                    sleep(5000)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                } finally {
+                    startActivity(toLogin)
+                    finish()
+                }
+            }
+        }
+        timer.start()
     }
 }
