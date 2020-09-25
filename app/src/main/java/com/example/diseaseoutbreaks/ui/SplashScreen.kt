34e430b1c1fbc5bffe3bd1d26ca20fe2d165c3ae
MosesWangira <40572173+MosesWangira@users.noreply.github.com>
2020-09-25 19:21:50 +0300

    splash animation added

--- app/src/main/java/com/example/diseaseoutbreaks/ui/SplashScreen.kt
+++ app/src/main/java/com/example/diseaseoutbreaks/ui/SplashScreen.kt
@@ -8,5 +8,7 @@ class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+
     }
 }
