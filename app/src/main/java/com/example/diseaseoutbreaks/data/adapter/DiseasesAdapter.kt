9ac958ca026b1cfd3d6a1f0af1826ab4f66ae259
MosesWangira <40572173+MosesWangira@users.noreply.github.com>
2020-09-25 19:50:02 +0300

    remove animation

--- app/src/main/java/com/example/diseaseoutbreaks/data/adapter/DiseasesAdapter.kt
+++ app/src/main/java/com/example/diseaseoutbreaks/data/adapter/DiseasesAdapter.kt
@@ -35,33 +35,10 @@ class DiseasesAdapter(val items: DataClass) :
 
         holder.view.title.text = disease.title
         holder.view.publication_date.text = "Publication Date : ${disease.pubDate}"
-//        holder.view.link.text = "Link : ${disease.link}"
         holder.view.description.text = disease.description
-
-        setAnimation(holder.itemView, position)
-
     }
 
 
     class DiseaseViewHolder(val view: View) : RecyclerView.ViewHolder(view)
 
-    private fun setAnimation(
-        viewToAnimate: View,
-        position: Int
-    ) {
-            val anim = ScaleAnimation(
-                0.0f,
-                1.0f,
-                0.0f,
-                1.0f,
-                Animation.RELATIVE_TO_SELF,
-                0.5f,
-                Animation.RELATIVE_TO_SELF,
-                0.5f
-            )
-            //anim.setDuration(new Random().nextInt(501));//to make duration random number between [0,501)
-            anim.duration = 600 //to make duration random number between [0,501)
-            viewToAnimate.startAnimation(anim)
-            lastPosition = position
-    }
 }
