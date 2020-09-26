163ad018fada4ca26b45d600d3f9d98f752bfd16
MosesWangira <40572173+MosesWangira@users.noreply.github.com>
2020-09-26 17:29:49 +0300

    initialize news view model

---
+++ app/src/main/java/com/example/diseaseoutbreaks/ui/news/NewsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.diseaseoutbreaks.ui.news
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class NewsViewModel(application: Application): AndroidViewModel(application) {
+}
