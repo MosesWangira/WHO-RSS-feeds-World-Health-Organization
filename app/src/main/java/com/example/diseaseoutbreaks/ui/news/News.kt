57092c9651367aa50085fb35299cab71e6365053
MosesWangira <40572173+MosesWangira@users.noreply.github.com>
2020-09-28 21:45:04 +0300

    use onViewCreated instead of onCreateView

--- app/src/main/java/com/example/diseaseoutbreaks/ui/news/News.kt
+++ app/src/main/java/com/example/diseaseoutbreaks/ui/news/News.kt
@@ -1,14 +1,11 @@
 package com.example.diseaseoutbreaks.ui.news
 
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import com.example.diseaseoutbreaks.R
 import com.example.diseaseoutbreaks.data.Model.news.NewsDataClass
 import com.example.diseaseoutbreaks.databinding.FragmentNewsBinding
@@ -16,33 +13,25 @@ import com.example.diseaseoutbreaks.util.animate
 import com.example.diseaseoutbreaks.util.toast
 import com.example.diseaseoutbreaks.viewmodels.NewsViewModel
 
-class News : Fragment() {
+class News : Fragment(R.layout.fragment_news) {
 
     lateinit var binding: FragmentNewsBinding
-
     lateinit var viewModel: NewsViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_news, container, false)
-
-        Log.d("Disease: ", "Disease ViewModel Called")
-        @Suppress("DEPRECATION")
-        viewModel = ViewModelProviders.of(this).get(NewsViewModel::class.java)
-
-        binding.fragmentNewsViewModel = viewModel
-        binding.lifecycleOwner = this
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = DataBindingUtil.bind(view)!!
+        viewModel = ViewModelProvider(this).get(NewsViewModel::class.java)
+        binding.apply {
+            fragmentNewsViewModel = viewModel
+            lifecycleOwner = this@News
+        }
 
         makeApiCallCoroutine()
-        return binding.root
     }
 
     private fun makeApiCallCoroutine(): NewsViewModel {
-        @Suppress("DEPRECATION")
-        viewModel.getAllNewsData().observe(this, Observer<NewsDataClass> {
+        viewModel.getAllNewsData().observe(viewLifecycleOwner, Observer<NewsDataClass> {
             if (it != null) {
                 /**
                  * update the adapter
